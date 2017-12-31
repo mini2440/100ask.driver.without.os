ba7dacb62b1953ad145ea0903095db5e658c77fe
mz8023yt <mz8023yt@163.com>
2017-12-31 23:17:14 +0800

    style: format the led driver

--- s3c2440a.gpio.led/main.c
+++ s3c2440a.gpio.led/main.c
@@ -2,8 +2,22 @@
 #include "bsp_led.h"
 #include "bsp_delay.h"
 
+/* 定义 LCD 对应 GPIO 的控制器寄存器 */
+#define GPGCON          REG(0x56000060)
+#define GPGDAT          REG(0x56000064)
+
 int main()
 {
+        /* 设置 LCD PWR 对应的 GPIO 为输出模式 */
+        /* 0000 0000 | 0000 0000 | 0000 0011 | 0000 0000 */
+        GPGCON &= ~0x00000300;
+        /* 0000 0000 | 0000 0000 | 0000 0001 | 0000 0000 */
+        GPGCON |= 0x00000100;
+
+        /* 设置 LCD PWR 对应的 GPIO 为输出 0 */
+        /* 0000 0000 | 0000 0000 | 0000 0000 | 0001 0000 */
+        GPGDAT &= ~0x00000010;
+
         led_init();
         while(1)
         {
--- s3c2440a.gpio.led/startup.s
+++ s3c2440a.gpio.led/startup.s
@@ -5,7 +5,7 @@ _start:
         ldr     r0,     =0x53000000     @ 关闭看门狗
         mov     r1,     #0x0
         str     r1,     [r0]
-        ldr     lr,     =halt_loop      @ 设置 main 函数返回地址
-        ldr     pc,     =main           @ 调用 SDRAM 中的 main 函数
-halt_loop:
-        b       halt_loop
+        ldr     lr,     =loop           @ 设置 main 函数返回地址
+        ldr     pc,     =main           @ 调用 SRAM 中的 main 函数
+loop:
+        b       loop
